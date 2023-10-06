bc6feaf54b1911c6edd016513c7a2ce6f206556d
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-10-06 20:33:01 +0300

    Edabit - a couple of more easy challenges

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
+
         System.out.println("Please choose a resources: " +
                 "1. Leetcode" +
-                "2. Edabit");
+                "2. Edabit" +
+                "3. HackerRank");
+
+
+        //TODO make user interface
+//        int siteChoice = Integer.parseInt(scanner.nextLine());
+//        switch (siteChoice) {
+//            case 1:
+//                handleLeetcode();
+//                break;
+//
+//            case 2:
+////                handleEdabit();
+//            default:
+//                System.out.println("Try again.");
+//        }
 
 
     }
+
+    private static void handleLeetcode() {
+        System.out.println("Leetcode menu not implemented yet.");
+    }
+
+
 }
